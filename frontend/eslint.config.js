92f3a1df619bb8b040ca1321e55523207b785c01
Nadezhda Oglezneva <noglezneva77@gmail.com>
2026-08-21 19:56:48 +0300

    ci: add frontend lint and build checks

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -7,19 +7,33 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+    ],
+
     files: ["**/*.{ts,tsx}"],
+
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
+
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true,
+          allowExportNames: ["useAuth"],
+        },
+      ],
     },
   },
 );
